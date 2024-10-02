0c5e9595553edf519a662468356acd7cfef2ded9
suaib islam <suaibs589@gmail.com>
2024-10-02 21:42:04 +0600

    accordingadd

--- src/app/components/accordion/page.jsx
+++ src/app/components/accordion/page.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import "./style.css";
 
-const Accordions = ({ title, info }) => {
+const Accordions = ({ title:any, info:any }) => {
   const [data, setdata] = useState(false);
   return (
     <>
